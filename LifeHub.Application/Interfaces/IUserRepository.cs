e6e713435efba20739ca58f91994679412ad94e2
blazejrutkowski <blazejrutkowskiwork.gmail.com>
2026-06-14 17:43:50 +0200

    clean user repo methods

--- LifeHub.Application/Interfaces/IUserRepository.cs
+++ LifeHub.Application/Interfaces/IUserRepository.cs
@@ -1,12 +1,10 @@
-﻿using LifeHub.Application.DTOs;
-using LifeHub.Domain.Entity;
+﻿using LifeHub.Domain.Entity;
 
 namespace LifeHub.Application.Interfaces
 {
     public interface IUserRepository
     {
         Task<User?> GetById(Guid id, CancellationToken cancellationToken = default);
-        Task<User?> GetByUsername(string username, CancellationToken cancellationToken = default);
         Task<User?> GetByEmail(string email, CancellationToken cancellationToken = default);
         Task Add(User user, CancellationToken cancellationToken = default);
         Task Update(User user, CancellationToken cancellationToken = default);
